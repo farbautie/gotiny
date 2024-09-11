2aa1c11ddc8bc7abd49af4f4edb048b65fa61ddf
Camilo Vargas <ca06vargas@gmail.com>
2024-09-11 00:41:59 -0500

    feat: :sparkles: basic routes started

--- internal/app/handlers/shorten.go
+++ internal/app/handlers/shorten.go
@@ -1,1 +1,11 @@
 package handlers
+
+import "net/http"
+
+func (h *Handler) ShortenUrl(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) GetShortenUrl(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) UpdateShortenUrl(w http.ResponseWriter, r *http.Request) {}
+
+func (h *Handler) DeleteShortenUrl(w http.ResponseWriter, r *http.Request) {}
--- internal/app/router.go
+++ internal/app/router.go
@@ -1,9 +1,19 @@
 package app
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/farbautie/gotiny/internal/app/handlers"
+)
 
 func NewRouter() *http.ServeMux {
 	router := http.NewServeMux()
+	handlers := handlers.New()
+
+	router.HandleFunc("GET /api/v1/shorten/{shorten_url}", handlers.GetShortenUrl)
+	router.HandleFunc("POST /api/v1/shorten", handlers.ShortenUrl)
+	router.HandleFunc("PUT /api/v1/shorten/{shorten_url}", handlers.UpdateShortenUrl)
+	router.HandleFunc("DELETE /api/v1/shorten/{shorten_url}", handlers.DeleteShortenUrl)
 
 	return router
 }
